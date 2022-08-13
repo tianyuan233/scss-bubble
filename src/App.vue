5d90fa44cc82fa83517d36c8ae537090efa1ce0c
tianyuan233 <zty.dev@outlook.com>
2022-08-13 22:43:28 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,269 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div class="g-wrap">
+    <div class="g-footer">
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+      <div class="g-bubble"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.g-wrap {
+  height: 100vh;
+  width: 100vw;
+  //key
+  filter: contrast(60);
+  background: #fff;
+  position: relative;
+}
+
+.g-footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 50vh;
+  width: 100%;
+  background: blue;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: -300px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
+    //key
+    backdrop-filter: blur(20px);
+  }
+}
+
+@for $i from 0 through 200 {
+  .g-bubble:nth-child(#{$i}) {
+    position: absolute;
+    background: blue;
+    $width: random(200)+px;
+    left: calc(#{(random(100)) + '%'} - #{$width});
+    top: #{(random(100))}px;
+    width: $width;
+    height: $width;
+    animation: moveToTop #{(random(2500) + 1500) / 1000}s ease-in-out -#{random(5000)/1000}s infinite;
+    overflow: hidden;
+    border-radius: 100%;
+  }
+}
+
+@keyframes moveToTop {
+  90% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: .08;
+    transform: translate(0, -200px) scale(.4);
+  }
+}
+</style>
